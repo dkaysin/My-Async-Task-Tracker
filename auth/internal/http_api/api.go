1d63677f356f525f229cebe068fce4dba7e07baa
Dmitry Kaysin <kaysin.dmitry@gmail.com>
2024-02-24 23:31:05 +0200

    move requireRoles to api file

--- auth/internal/http_api/api.go
+++ auth/internal/http_api/api.go
@@ -3,9 +3,12 @@ package http_handler
 import (
 	"async_course/auth"
 	service "async_course/auth/internal/service"
+	"log/slog"
 	"net/http"
+	"slices"
 
 	"github.com/go-playground/validator"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
 )
@@ -50,4 +53,32 @@ func ResponseError(err error) map[string]interface{} {
 
 func JwtMiddlewareErrorHandler(c echo.Context, err error) error {
 	return c.JSON(http.StatusForbidden, ResponseError(err))
+}
+
+func (h *HttpAPI) requireRoles(fn echo.HandlerFunc, roles []string) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token, ok := c.Get("user").(*jwt.Token)
+		if !ok {
+			slog.Error("could not find jwt token in request context", "error", auth.ErrTokenNotFound)
+			return c.JSON(http.StatusInternalServerError, auth.ErrTokenNotFound)
+		}
+
+		claims, ok := token.Claims.(*auth.JwtCustomClaims)
+		if !ok {
+			slog.Error("cannot cast to *jwtClaims", "error", auth.ErrInvalidJwtClaimsFormat)
+			return c.JSON(http.StatusForbidden, auth.ErrInvalidJwtClaimsFormat)
+		}
+
+		if claims == nil {
+			slog.Error("empty claims in provided token", "error", auth.ErrInvalidJwtClaimsFormat)
+			return c.JSON(http.StatusForbidden, auth.ErrInvalidJwtClaimsFormat)
+		}
+
+		role := claims.Role
+		if !slices.Contains(roles, role) {
+			return c.JSON(http.StatusForbidden, ResponseError(auth.ErrInsufficientPrivileges))
+		}
+		return fn(c)
+	}
+
 }
