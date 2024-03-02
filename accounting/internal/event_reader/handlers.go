9074894011f7afd2f4b6e6bde4dd9e923e067ef5
Dmitry Kaysin <kaysin.dmitry@gmail.com>
2024-03-02 23:10:40 +0200

    improve event reader logs

--- accounting/internal/event_reader/handlers.go
+++ accounting/internal/event_reader/handlers.go
@@ -25,6 +25,7 @@ func (er *EventReader) handleMessage(m kafka.Message) error {
 		slog.Error("errorw while unmarshaling event", "err", err)
 		return err
 	}
+	slog.Info("parsed raw event", "event_name", eventRaw.EventName, "event_version", eventRaw.EventVersion, "event_producer", eventRaw.EventProducer)
 
 	switch eventRaw.EventName {
 	case schema.EventNameAccountCreated:
